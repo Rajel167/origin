3601ec482842246b595ba08f1b977bf5da51dccd
DESKTOP-1723V22\P15 <egor.sokkologorsky@gmail.com>
2023-04-22 14:45:36 +0300

    arduino code

---
+++ sketch_apr11e/sketch_apr11e.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(analogRead(A1));
+  if (analogRead(A1)>4){
+    digitalWrite(3,1);
+    delay(1000);
+    digitalWrite(3,0);
+  }
+}
